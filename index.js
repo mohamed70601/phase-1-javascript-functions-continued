ff388d661841072bd018100fbceef53d88b8b14c
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-08-06 23:49:21 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+function wrapAdjective(flair = "*") {
+    return function inner(para = "special") {
+        return `You are ${flair}${para}${flair}!`;
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
